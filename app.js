6b7ed5bb3656f2b26979a1a0fb285a75f4b07115
Jason <jcvalle@mail.usf.edu>
2014-08-20 21:18:16 -0500

    modularize the mail listener, and other small changes

--- app.js
+++ app.js
@@ -1,61 +1,24 @@
-var MailListener = require('mail-listener2'),
-    Notification = require('node-notifier');
+var Notification = require('node-notifier'),
+    config = require('./config.js');
 
-var config = require('./config.js');
+if (config.gmailAccount) {
+    var imapListener = require('./modules/imap-listener'),
+        ml = imapListener.init(),
+        unreadMail = [];
 
-var mailListener = new MailListener({
-    username: config.gmailAccount,
-    password: config.gmailPassword,
-    host: 'imap.gmail.com',
-    port: 993,
-    tls: true,
-    tlsOptions: { rejectUnauthorized: false },
-    mailbox: 'INBOX',
-    markSeen: true,
-    fetchUnreadOnStart: false, // use it only if you want to get all unread email on lib start. Default is `false`,
-    mailParserOptions: {streamAttachments: true}, // options to be passed to mailParser lib.
-    attachments: true, // download attachments as they are encountered to the project directory
-    attachmentOptions: { directory: "attachments/" }
-});
+    ml.start();
 
-mailListener.start();
+    ml.on('server:connected', function () {
+        console.log('Imap Connected');
+    });
 
-mailListener.on('server:connected', function () {
-    console.log('iMapConnected');
-});
-
-mailListener.on('mail', function (mail, seqno, attributes) {
-    console.log('emailParsed', mail);
-    buildEmailNotice(mail, notify);
-});
-
-function buildEmailNotice (email, callback) {
-    var notice = {};
-
-    notice.title = 'E-mail from ' + email.from[0].name + ' (' + email.from[0].address + ')';
-    notice.message = email.subject;
-    notice.type = 'email';
-    if (email.priority !== 'normal') {
-        notice.sticky = true;
-    } else {
-        notice.sticky = false;
-    }
-
-    callback(notice);
+    ml.on('message', function (mail, seqno, attributes) {
+        console.log(seqno);
+        notify(imapListener.buildEmailNotice(mail));
+    });
 }
 
 function notify (notice) {
-    var notifier = new Notification();
-
-    if (notice.type === 'email') {
-        notice.iconDir = __dirname + '/images/gmail.png';
-    }
-    
-    notifier.notify({
-        title: notice.title,
-        message: notice.message,
-        icon: notice.iconDir,
-        sticky: notice.sticky,
-        callbackTarget: 'http://mail.google.com'
-    });
+    var notifier = new Notification();    
+    notifier.notify(notice);
 }
---
+++ modules/imap-listener.js
@@ -0,0 +1,40 @@
+var MailListener = require('mail-listener2'),
+    config = require('../config.js');
+
+var imapListener = {};
+
+// function to generate our new mailListener Instance
+imapListener.init = function () {
+    return new MailListener({
+        username: config.gmailAccount,
+        password: config.gmailPassword,
+        host: 'imap.gmail.com',
+        port: 993,
+        tls: true,
+        tlsOptions: { rejectUnauthorized: false },
+        mailbox: 'INBOX',
+        markSeen: false,
+        fetchUnreadOnStart: false, // use it only if you want to get all unread email on lib start. Default is `false`,
+        mailParserOptions: {streamAttachments: true}, // options to be passed to mailParser lib.
+        attachments: true, // download attachments as they are encountered to the project directory
+        attachmentOptions: { directory: "attachments/" }
+    });
+};
+
+imapListener.buildEmailNotice = function (email, callback) {
+    var notice = {};
+
+    notice.title = 'E-mail from ' + email.from[0].name + ' (' + email.from[0].address + ')';
+    notice.message = email.subject;
+    notice.iconDir = __dirname + '/images/gmail.png';
+
+    if (email.priority !== 'normal') {
+        notice.sticky = true;
+    } else {
+        notice.sticky = false;
+    }
+
+    return notice;
+};
+
+module.exports = imapListener;
